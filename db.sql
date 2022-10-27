57d3047dcc337ac05eda501a821ec50740bc4010
Rostislav <rostik057@gmail.com>
2022-10-27 13:30:44 +0300

    database

--- db.sql
+++ db.sql
@@ -1,3 +1,7 @@
+--
+-- Структура таблицы `application`
+--
+
 CREATE TABLE `application` (
   `application_id` int(11) NOT NULL,
   `from` varchar(255) NOT NULL,
@@ -5,11 +9,12 @@ CREATE TABLE `application` (
   `date` date NOT NULL,
   `date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_edited` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `transport_type` varchar(255) NOT NULL,
+  `transport_type` int(11) NOT NULL,
   `user_fullname` varchar(60) NOT NULL,
   `user_telephone` int(30) DEFAULT NULL,
   `user_email` varchar(60) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT '1',
   `loading_method` int(11) DEFAULT NULL,
   `size` int(11) DEFAULT NULL,
   `height` int(11) DEFAULT NULL,
@@ -24,9 +29,8 @@ CREATE TABLE `application` (
 -- Дамп данных таблицы `application`
 --
 
-INSERT INTO `application` (`application_id`, `from`, `to`, `date`, `date_created`, `date_edited`, `transport_type`, `user_fullname`, `user_telephone`, `user_email`, `user_id`, `loading_method`, `size`, `height`, `photo`, `mass`, `price`, `comment`, `is_deleted`) VALUES
-(1, 'Россия', 'Россия', '2022-10-20', '2022-10-21 13:06:37', '2022-10-21 13:06:37', 'Тяжелая', 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, NULL, NULL, NULL, NULL, NULL, 3221, NULL, 0),
-(2, 'Россия', 'Россия', '2022-10-07', '2022-10-23 12:26:36', '2022-10-23 12:26:36', 'Легковой автомобиль', 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, NULL, NULL, NULL, '16665171969845.jpeg', NULL, NULL, 'Срочный заказ', 0);
+INSERT INTO `application` (`application_id`, `from`, `to`, `date`, `date_created`, `date_edited`, `transport_type`, `user_fullname`, `user_telephone`, `user_email`, `user_id`, `status`, `loading_method`, `size`, `height`, `photo`, `mass`, `price`, `comment`, `is_deleted`) VALUES
+(2, 'Россия', 'Россия', '2022-10-07', '2022-10-23 12:26:36', '2022-10-27 01:47:57', 1, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, 1, NULL, NULL, '16668244778708.png', NULL, 1, 'Срочный заказ', 0);
 
 -- --------------------------------------------------------
 
@@ -39,6 +43,15 @@ CREATE TABLE `height` (
   `name` varchar(255) NOT NULL
 );
 
+--
+-- Дамп данных таблицы `height`
+--
+
+INSERT INTO `height` (`height_id`, `name`) VALUES
+(1, '1.5'),
+(2, '2'),
+(3, '2.5');
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +62,20 @@ CREATE TABLE `loading_method` (
   `loading_method_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL
 );
+
+--
+-- Дамп данных таблицы `loading_method`
+--
+
+INSERT INTO `loading_method` (`loading_method_id`, `name`) VALUES
+(1, 'With the removal of crossbars'),
+(2, 'Without a gate'),
+(3, 'With the removal of racks'),
+(4, 'Upper'),
+(5, 'Side'),
+(6, 'Rear'),
+(7, 'With full rastentovka'),
+(8, 'Hydroboard');
 
 -- --------------------------------------------------------
 
@@ -66,9 +93,98 @@ CREATE TABLE `size` (
 --
 
 INSERT INTO `size` (`size_id`, `name`) VALUES
-(1, '1'),
-(2, '3'),
-(3, '5');
+(1, '48 X 48'),
+(2, '48 X 45'),
+(3, '48 X 40'),
+(4, 'Other');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+);
+
+--
+-- Дамп данных таблицы `status`
+--
+
+INSERT INTO `status` (`status_id`, `name`) VALUES
+(1, 'Posted'),
+(2, 'In Search'),
+(3, 'Posted'),
+(4, 'In Search'),
+(5, 'ooked'),
+(6, 'On the way to pickup'),
+(7, 'Carrier loading'),
+(8, 'On the way to destination'),
+(9, 'Unloading'),
+(10, 'Delivered'),
+(11, 'Awaiting quote'),
+(12, 'Quoted');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `transport_type`
+--
+
+CREATE TABLE `transport_type` (
+  `transport_type_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+);
+
+--
+-- Дамп данных таблицы `transport_type`
+--
+
+INSERT INTO `transport_type` (`transport_type_id`, `name`) VALUES
+(1, 'Awning'),
+(2, 'Celomyant'),
+(3, 'Beads'),
+(4, 'Container'),
+(5, 'Clothing truck'),
+(6, 'Isotherm'),
+(7, 'Ref.'),
+(8, 'Ref.-tushevoz'),
+(9, 'Car carrier'),
+(10, 'Concrete truck'),
+(11, 'Grain carrier'),
+(12, 'Logging truck'),
+(13, 'Horse carrier'),
+(14, 'Crane'),
+(15, 'Garbage truck'),
+(16, 'Loader'),
+(17, 'Poultry truck'),
+(18, 'Cattle truck'),
+(19, 'Pipe carrier'),
+(20, 'Tractor'),
+(21, 'Woodchip truck'),
+(22, 'Tow truck'),
+(23, 'Yacht carrier'),
+(24, 'Onboard/Open'),
+(25, 'Platform'),
+(26, 'Manipulator'),
+(27, 'Scrap truck/Metal truck'),
+(28, 'Container ship'),
+(29, 'Trawl/Oversized'),
+(30, 'Plitovoz'),
+(31, 'Dump truck'),
+(32, 'Minibus'),
+(33, 'Bus'),
+(34, 'Milk truck'),
+(35, 'Bitumen truck'),
+(36, 'Fuel tanker'),
+(37, 'Gas carrier'),
+(38, 'Feed truck'),
+(39, 'Flour truck'),
+(40, 'Tanker truck'),
+(41, 'Cement truck'),
+(42, 'Maslovoz');
 
 -- --------------------------------------------------------
 
@@ -96,7 +212,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `email`, `telephone`, `password`, `name`, `surname`, `avatar`, `created`, `user_type_id`, `last_online`, `is_online`, `is_banned`) VALUES
-(1, 'rostik057@gmail.com', '79999999', '$2y$10$bsQTdXVTQ0Q46j8ljpKPXOfluHPuF3pXoS.BudiL0voc2GEX6SdqS', 'Ростислав', 'Фамилия', NULL, '2022-10-15 22:27:42', 1, '2022-10-15 22:27:42', 1, 0),
+(1, 'rostik057@gmail.com', '79999999', '$2y$10$bsQTdXVTQ0Q46j8ljpKPXOfluHPuF3pXoS.BudiL0voc2GEX6SdqS', 'Ростислав', 'Фамилия', '16668247037096.jpeg', '2022-10-15 22:27:42', 1, '2022-10-15 22:27:42', 1, 0),
 (2, 'zajcevav30@gmail.com', '79999999999', '$2y$10$HPWufvrFTHNVH.iqJm3wb.p1KIgQK9Bx1RN2B0MrkpyBy3RestyTy', 'zajcevav301', 'zajcevav301', NULL, '2022-10-17 16:00:48', 1, '2022-10-17 16:00:48', 1, 0);
 
 -- --------------------------------------------------------
@@ -128,7 +244,13 @@ INSERT INTO `user_session` (`user_session_id`, `token`, `date`, `user_id`) VALUE
 -- Индексы таблицы `application`
 --
 ALTER TABLE `application`
-  ADD PRIMARY KEY (`application_id`);
+  ADD PRIMARY KEY (`application_id`),
+  ADD KEY `status` (`status`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `loading_method` (`loading_method`),
+  ADD KEY `size` (`size`),
+  ADD KEY `height` (`height`),
+  ADD KEY `transport_type` (`transport_type`);
 
 --
 -- Индексы таблицы `height`
@@ -147,6 +269,18 @@ ALTER TABLE `loading_method`
 --
 ALTER TABLE `size`
   ADD PRIMARY KEY (`size_id`);
+
+--
+-- Индексы таблицы `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Индексы таблицы `transport_type`
+--
+ALTER TABLE `transport_type`
+  ADD PRIMARY KEY (`transport_type_id`);
 
 --
 -- Индексы таблицы `user`
@@ -174,19 +308,31 @@ ALTER TABLE `application`
 -- AUTO_INCREMENT для таблицы `height`
 --
 ALTER TABLE `height`
-  MODIFY `height_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `height_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `loading_method`
 --
 ALTER TABLE `loading_method`
-  MODIFY `loading_method_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `loading_method_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT для таблицы `size`
 --
 ALTER TABLE `size`
-  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT для таблицы `transport_type`
+--
+ALTER TABLE `transport_type`
+  MODIFY `transport_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT для таблицы `user`
@@ -199,4 +345,23 @@ ALTER TABLE `user`
 --
 ALTER TABLE `user_session`
   MODIFY `user_session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `application`
+--
+ALTER TABLE `application`
+  ADD CONSTRAINT `application_ibfk_1` FOREIGN KEY (`status`) REFERENCES `status` (`status_id`),
+  ADD CONSTRAINT `application_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  ADD CONSTRAINT `application_ibfk_3` FOREIGN KEY (`loading_method`) REFERENCES `loading_method` (`loading_method_id`),
+  ADD CONSTRAINT `application_ibfk_4` FOREIGN KEY (`size`) REFERENCES `size` (`size_id`),
+  ADD CONSTRAINT `application_ibfk_5` FOREIGN KEY (`height`) REFERENCES `height` (`height_id`),
+  ADD CONSTRAINT `application_ibfk_6` FOREIGN KEY (`transport_type`) REFERENCES `transport_type` (`transport_type_id`);
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
