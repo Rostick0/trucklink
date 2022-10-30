2163bf1748c4363deb5eeeccc4f0c1a0bac0688f
Rostislav <rostik057@gmail.com>
2022-10-30 16:35:47 +0300

    db user_type

--- db.sql
+++ db.sql
@@ -1,6 +1,3 @@
---
--- Структура таблицы `application`
---
 
 CREATE TABLE `application` (
   `application_id` int(11) NOT NULL,
@@ -30,7 +27,11 @@ CREATE TABLE `application` (
 --
 
 INSERT INTO `application` (`application_id`, `from`, `to`, `date`, `date_created`, `date_edited`, `transport_type`, `user_fullname`, `user_telephone`, `user_email`, `user_id`, `status`, `loading_method`, `size`, `height`, `photo`, `mass`, `price`, `comment`, `is_deleted`) VALUES
-(2, 'Россия', 'Россия', '2022-10-07', '2022-10-23 12:26:36', '2022-10-27 01:47:57', 1, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, 1, NULL, NULL, '16668244778708.png', NULL, 1, 'Срочный заказ', 0);
+(1, 'Россия', 'Россия', '2022-10-20', '2022-10-21 13:06:37', '2022-10-21 13:06:37', 1, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, 1, 1, 1, NULL, 1, 3221, NULL, 0),
+(2, 'Россия', 'Россия', '2022-10-07', '2022-10-23 12:26:36', '2022-10-23 12:26:36', 1, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, NULL, NULL, NULL, '16665171969845.jpeg', NULL, NULL, 'Срочный заказ', 0),
+(3, 'Рязань', 'Ленинский проспект', '2022-10-30', '2022-10-26 20:41:47', '2022-10-26 20:41:47', 1, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0),
+(4, 'Теско Екстра', 'Санкт-Петербург', '2022-10-29', '2022-10-26 20:42:47', '2022-10-26 20:42:47', 5, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, NULL, NULL, NULL, '16668061677057.jpeg', NULL, 154, 'Срочный заказ', 0),
+(7, 'Украина', 'Bibigon', '2022-11-20', '2022-10-29 03:05:15', '2022-10-29 03:05:15', 31, 'Ростислав Фамилия', 79999999, 'rostik057@gmail.com', 1, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -118,7 +119,7 @@ INSERT INTO `status` (`status_id`, `name`) VALUES
 (2, 'In Search'),
 (3, 'Posted'),
 (4, 'In Search'),
-(5, 'ooked'),
+(5, 'Looked'),
 (6, 'On the way to pickup'),
 (7, 'Carrier loading'),
 (8, 'On the way to destination'),
@@ -213,7 +214,7 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`user_id`, `email`, `telephone`, `password`, `name`, `surname`, `avatar`, `created`, `user_type_id`, `last_online`, `is_online`, `is_banned`) VALUES
 (1, 'rostik057@gmail.com', '79999999', '$2y$10$bsQTdXVTQ0Q46j8ljpKPXOfluHPuF3pXoS.BudiL0voc2GEX6SdqS', 'Ростислав', 'Фамилия', '16668247037096.jpeg', '2022-10-15 22:27:42', 1, '2022-10-15 22:27:42', 1, 0),
-(2, 'zajcevav30@gmail.com', '79999999999', '$2y$10$HPWufvrFTHNVH.iqJm3wb.p1KIgQK9Bx1RN2B0MrkpyBy3RestyTy', 'zajcevav301', 'zajcevav301', NULL, '2022-10-17 16:00:48', 1, '2022-10-17 16:00:48', 1, 0);
+(2, 'zajcevav30@gmail.com', '79999999999', '$2y$10$HPWufvrFTHNVH.iqJm3wb.p1KIgQK9Bx1RN2B0MrkpyBy3RestyTy', 'zajcevav301', 'zajcevav301', NULL, '2022-10-17 16:00:48', 2, '2022-10-17 16:00:48', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -235,6 +236,27 @@ CREATE TABLE `user_session` (
 INSERT INTO `user_session` (`user_session_id`, `token`, `date`, `user_id`) VALUES
 (1, 'b5365908c694777ed8292ffd99ac9049', '2022-10-15 22:27:42', 1),
 (2, 'e3c38c6c4e8e4e078a0b8646778edcda', '2022-10-17 16:00:48', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_type`
+--
+
+CREATE TABLE `user_type` (
+  `user_type_id` int(11) NOT NULL,
+  `name` varchar(63) NOT NULL,
+  `power` int(11) NOT NULL
+);
+
+--
+-- Дамп данных таблицы `user_type`
+--
+
+INSERT INTO `user_type` (`user_type_id`, `name`, `power`) VALUES
+(1, 'user', 1),
+(2, 'moderator', 10),
+(3, 'admin', 25);
 
 --
 -- Индексы сохранённых таблиц
@@ -286,13 +308,20 @@ ALTER TABLE `transport_type`
 -- Индексы таблицы `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `user_type_id` (`user_type_id`);
 
 --
 -- Индексы таблицы `user_session`
 --
 ALTER TABLE `user_session`
   ADD PRIMARY KEY (`user_session_id`);
+
+--
+-- Индексы таблицы `user_type`
+--
+ALTER TABLE `user_type`
+  ADD PRIMARY KEY (`user_type_id`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -302,7 +331,7 @@ ALTER TABLE `user_session`
 -- AUTO_INCREMENT для таблицы `application`
 --
 ALTER TABLE `application`
-  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `application_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `height`
@@ -347,6 +376,12 @@ ALTER TABLE `user_session`
   MODIFY `user_session_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT для таблицы `user_type`
+--
+ALTER TABLE `user_type`
+  MODIFY `user_type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
@@ -360,6 +395,12 @@ ALTER TABLE `application`
   ADD CONSTRAINT `application_ibfk_4` FOREIGN KEY (`size`) REFERENCES `size` (`size_id`),
   ADD CONSTRAINT `application_ibfk_5` FOREIGN KEY (`height`) REFERENCES `height` (`height_id`),
   ADD CONSTRAINT `application_ibfk_6` FOREIGN KEY (`transport_type`) REFERENCES `transport_type` (`transport_type_id`);
+
+--
+-- Ограничения внешнего ключа таблицы `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`user_type_id`) REFERENCES `user_type` (`user_type_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
